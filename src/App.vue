9e223417dca482dd41f191de015f1f68e8aa5710
wangshanshan <821051557@qq.com>
2023-07-20 09:21:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-row>
+      <yqytest :data="sellCardList" />
+    </el-row>
+  </div>
+</template>
+
+<script>
+import yqytest from "./components/yqyTest.vue";
+export default {
+  components: {
+    yqytest,
+  },
+  data() {
+    return {
+      sellCardList: [],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
